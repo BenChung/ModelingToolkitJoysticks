38119fc025c81c0a11f75130e870af02312ad436
Ben Chung <ckfinite@gmail.com>
2025-03-20 13:42:31 -0700

    Add frame limiter, fix a few bugs

--- src/ModelingToolkitJoysticks.jl
+++ src/ModelingToolkitJoysticks.jl
@@ -1,13 +1,14 @@
 module ModelingToolkitJoysticks
-using ModelingToolkit, GLFW, Symbolics, Setfield
+using ModelingToolkit, GLFW, Symbolics, Setfield, Dates
 using ModelingToolkit: t_nounits as t, D_nounits as D
 
 @component function Joystick(joystick::GLFW.Joystick; name, update_rate=1/60)
     naxes = length(GLFW.GetJoystickAxes(joystick))
-    pars = [(parameters $(Symbol("axis$i"))(t))[1] for i ∈ 1:naxes]
+    names = [Symbol("axis$i") for i ∈ 1:naxes]
+    pars = [(@parameters $(n)(t)=0.0)[1] for n ∈ names]
     return ODESystem(
-        [], t, [], pars; name=name,
-        discrete_events = [update_rate => ModelingToolkit.ImperativeAffect(modified = pars) do m, _, _, _
+        Equation[], t, [], pars; name=name,
+        discrete_events = [update_rate => ModelingToolkit.ImperativeAffect(modified = NamedTuple{(names...,)}(pars)) do m, _, _, _
             return NamedTuple{keys(m)}(GLFW.GetJoystickAxes(joystick))
         end]
     )
@@ -19,25 +20,33 @@ end
 get_joystick_by_number(num) = if num >= 1 && num <= 16 GLFW.Joystick(num - 1) else throw("Invalid joystick number $num") end
 
 
-@component function FrameLimiter(;name, frametime_target=1/60, sleep_guess=frametime_target, onupdate=nothing)
-    last_time = nothing
-    function limiter(integ, u, p, ctx)
-        if isnothing(last_time)
-            last_time = now()
-            return
+@component function FrameLimiter(;name, 
+    frametime_target=1/60, sleep_guess=frametime_target, onupdate=nothing,
+    observed=(;))
+    @parameters dt_act(t)=0.0 last_time(t)=NaN
+    epoch = now()
+    # todo: fix empty return
+    enforce_frametime = ModelingToolkit.ImperativeAffect(; modified = (;dt_act, last_time), observed) do m,o,_,integ
+        dt_act, last_time = m
+        if isnan(last_time)
+            last_time = (now() - epoch)/Second(1)
+            return (;dt_act, last_time)
         end
-        current_time = now()
+        current_time = (now() - epoch)/Second(1)
         dt = current_time - last_time
         dt_err = frametime_target - dt
         last_time = current_time
         if !isnothing(onupdate)
-            onupdate(integ)
+            onupdate(o, integ)
         end
-        sleep(sleep_guess - dt_err)
+        dt_act += dt_err * 0.05
+        @show integ.t
+        sleep(max(sleep_guess + dt_act, 0.0))
+        return (;dt_act, last_time)
     end
     return ODESystem(
-        [], t; name=name, 
-        discrete_events = [frametime_target => (limiter, [], [], [], nothing)]
+        Equation[], t, [], [dt_act, last_time]; name=name, 
+        discrete_events = [frametime_target => enforce_frametime]
     )
 end
 
